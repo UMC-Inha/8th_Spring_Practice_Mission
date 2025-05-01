a4bbe95c67022a0ef6a5d329ed055829700ba424
seungin <tmddls91@inha.edu>
2025-05-01 09:29:10 +0900

    INIT: 실습 레포지토리와 로컬 연결

---
+++ src/main/java/umc/UMC8th/Umc8thApplication.java
@@ -0,0 +1,13 @@
+package umc.UMC8th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc8thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc8thApplication.class, args);
+	}
+
+}
