68b0b599cfd13ea193e5c5e5d4e37c8fea9ad442
jlhyunii <jhpark4692@naver.com>
2025-05-01 22:23:53 +0900

    mission: submit week5 mission

---
+++ src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,36 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Float score;
+
+    @Column(nullable = false, length = 50)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
+}
