2d612b2615052fa47fd652eafab336fa0057d105
zjhj0814 <zjhj0814@gmail.com>
2025-05-13 22:27:01 +0900

    COMMENT: ExceptionAdvice 이해를 위한 주석 작성

--- src/main/java/umc/study/apiPayload/exception/ExceptionAdvice.java
+++ src/main/java/umc/study/apiPayload/exception/ExceptionAdvice.java
@@ -27,6 +27,7 @@ import java.util.Optional;
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
 
+    //ConstraintViolationException: @Validated 등에서 발생하는 유효성 검사 실패 예외
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
         String errorMessage = e.getConstraintViolations().stream()
@@ -37,21 +38,25 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
         return handleExceptionInternalConstraint(e, ErrorStatus.valueOf(errorMessage), HttpHeaders.EMPTY, request);
     }
 
-    @Override
+    @Override //Spring MVC가 @RequestBody 검증에 실패하면(@Valid) 어떤 필드가 실패했는지 명시적으로 알려주는 메서드 (기본 제공)
     public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
         Map<String, String> errors = new LinkedHashMap<>();
 
+        //BindingResult를 통해 에러 필드와 메시지 수집
         e.getBindingResult().getFieldErrors().stream()
                 .forEach(fieldError -> {
                     String fieldName = fieldError.getField();
                     String errorMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("");
+                    // fieldName이라는 키 값이 errors라는 LinkedHashMap에 이미 존재한다면 기존값과 새로운 값을 ,로 이어주고
+                    // 존재하지 않는다면 그냥 fieldName과 errorMessage를 key value로 errors에 저장한다.
                     errors.merge(fieldName, errorMessage, (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
                 });
 
         return handleExceptionInternalArgs(e, HttpHeaders.EMPTY, ErrorStatus.valueOf("_BAD_REQUEST"), request, errors);
     }
 
+    //예상하지 못한 모든 예외를 일괄 처리하는 메서드
     @ExceptionHandler
     public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         e.printStackTrace();
@@ -59,12 +64,25 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
         return handleExceptionInternalFalse(e, ErrorStatus._INTERNAL_SERVER_ERROR, HttpHeaders.EMPTY, ErrorStatus._INTERNAL_SERVER_ERROR.getHttpStatus(), request, e.getMessage());
     }
 
+    //개발자의 커스텀 예외(GeneratlException) 처리 메서드
     @ExceptionHandler(value = GeneralException.class)
     public ResponseEntity onThrowException(GeneralException generalException, HttpServletRequest request) {
         ErrorReasonDTO errorReasonHttpStatus = generalException.getErrorReasonHttpStatus();
         return handleExceptionInternal(generalException, errorReasonHttpStatus, null, request);
     }
 
+
+    /**
+     * 아래 handleExceptionInternal... 메서드 모두
+     * 알맞은 부모 클래스의 handleExceptionInternal을 통해 ResponseEntity<Object>를 반환하는 메서드이다.
+     * 따라서, 이 중복되는 4가지 메서드를 하나의 buildErrorResponse 메서드로 리팩토링할 수 있다.
+     *
+     * @param e
+     * @param reason
+     * @param headers
+     * @param request
+     * @return
+     */
     private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorReasonDTO reason,
                                                            HttpHeaders headers, HttpServletRequest request) {
 
