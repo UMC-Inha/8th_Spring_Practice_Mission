3ce27c51132f0c51a00a3bc5840bcacf83ed54b4
KiSeungMin <ksm011212@gmail.com>
2025-05-19 15:41:15 +0900

    FEAT: 1번 미션 구현 완료

--- src/main/java/umc/study/service/StoreService/StoreQueryService.java
+++ src/main/java/umc/study/service/StoreService/StoreQueryService.java
@@ -1,6 +1,7 @@
 package umc.study.service.StoreService;
 
 import umc.study.domain.Store;
+import umc.study.web.dto.Store.StoreRequestDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +10,5 @@ public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
     List<Store> findStoresByNameAndScore(String name, Float score);
+    Store registerStore(StoreRequestDto.JoinDto request);
 }
