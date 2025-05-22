8c17d08b3f19f96905f8da67ec5eba20f9d3884f
seungin <tmddls91@inha.edu>
2025-05-22 09:24:28 +0900

    FEAT: MemberRequest,MemberResponse DTO 구현

---
+++ src/main/java/umc/UMC8th/dto/MemberRequestDTO.java
@@ -0,0 +1,49 @@
+package umc.UMC8th.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+public class MemberRequestDTO {
+
+    @Getter
+    @NoArgsConstructor
+    public static class JoinDto {
+
+        @NotBlank
+        private String email;
+
+        @NotBlank
+        private String password; // 현재 나의 엔티티 필드에는 없는데 저장은 하지 않아도 있어야할거같아서 넣음
+
+        @NotBlank
+        private String userName;
+
+        @NotBlank
+        private String nickname;
+
+        @NotNull
+        private Integer userAge;
+
+        @NotBlank
+        private String userGender;
+
+        @NotBlank
+        private String userPhone;
+
+        @NotNull
+        private LocalDate userBirth;
+
+        @NotBlank
+        private String userAddress;
+
+        @NotBlank
+        private String foodCategories;
+
+        @NotBlank
+        private String region;
+    }
+}
