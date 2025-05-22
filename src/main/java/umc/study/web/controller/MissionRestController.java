31ca63276c2a430f0322b2ca2e84c29c260fe519
zjhj0814 <zjhj0814@gmail.com>
2025-05-22 21:44:31 +0900

    mission: 3번 가게에 미션 추가하기 API 구현

---
+++ src/main/java/umc/study/web/controller/MissionRestController.java
@@ -0,0 +1,28 @@
+package umc.study.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import umc.study.apiPayload.ApiResponse;
+import umc.study.converter.MissionConverter;
+import umc.study.domain.Mission;
+import umc.study.service.MissionService.MissionCommandService;
+import umc.study.validation.annotation.ExistStores;
+import umc.study.web.dto.MissionRequestDTO;
+import umc.study.web.dto.MissionResponseDTO;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class MissionRestController {
+    private final MissionCommandService missionCommandService;
+
+    @PostMapping("/api/v1/stores/{storeId}/missions")
+    public ApiResponse<MissionResponseDTO.createResultDTO> addMission(@PathVariable @ExistStores Long storeId, @RequestBody MissionRequestDTO.createMissionDTO request) {
+        Mission mission = missionCommandService.createMission(storeId, request);
+        return ApiResponse.onSuccess(MissionConverter.toCreateResultDTO(mission));
+    }
+}
