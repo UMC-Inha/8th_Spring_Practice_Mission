a41a2b46648629196b2e5d537a5f55d64371aa5b
seungin <tmddls91@inha.edu>
2025-05-22 14:30:55 +0900

    REFACTOR: Validator에서 서비스 계층 분리

--- src/main/java/umc/UMC8th/validation/validator/StoreExistValidator.java
+++ src/main/java/umc/UMC8th/validation/validator/StoreExistValidator.java
@@ -5,16 +5,17 @@ import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.UMC8th.repository.StoreRepository.StoreRepository;
+import umc.UMC8th.service.StoreValidationService;
 import umc.UMC8th.validation.annotation.StoreExists;
 
 @Component
 @RequiredArgsConstructor
 public class StoreExistValidator implements ConstraintValidator<StoreExists, Long> {
 
-    private final StoreRepository storeRepository;
+    private final StoreValidationService storeValidationService;
 
     @Override
     public boolean isValid(Long value, ConstraintValidatorContext context) {
-        return value != null && storeRepository.existsById(value);
+        return value != null && storeValidationService.isStoreExist(value);
     }
 }
