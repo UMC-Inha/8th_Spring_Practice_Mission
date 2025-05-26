49b067f096f1aec33f98b332e58aec6d848f3b34
KiSeungMin <ksm011212@gmail.com>
2025-05-26 17:09:53 +0900

    feat: 3번 미션 repository 로직 구현 완료

--- src/main/java/umc/study/repository/MemberRepository/MemberRepositoryCustom.java
+++ src/main/java/umc/study/repository/MemberRepository/MemberRepositoryCustom.java
@@ -1,8 +1,16 @@
 package umc.study.repository.MemberRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import umc.study.domain.mapping.MemberMission;
 import umc.study.web.dto.Member.MemberDetailResponseDto;
 
 public interface MemberRepositoryCustom {
 
     MemberDetailResponseDto findMemberDetail(Long memberId);
+
+    Page<MemberMission> findMissionsByMember(
+            Long memberId,
+            Pageable pageable
+    );
 }
