f7bd64d455ae7b90ae075ff3cfcebf591731dfac
KiSeungMin <ksm011212@gmail.com>
2025-05-26 16:38:48 +0900

    feat: 유저 Review 조회 repository 로직 구현 완료

--- src/main/java/umc/study/repository/ReviewRepository/ReviewRepositoryCustom.java
+++ src/main/java/umc/study/repository/ReviewRepository/ReviewRepositoryCustom.java
@@ -1,4 +1,9 @@
 package umc.study.repository.ReviewRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import umc.study.domain.Review;
+
 public interface ReviewRepositoryCustom {
+    Page<Review> findByMemberReviews(Long memberId, Pageable pageable);
 }
