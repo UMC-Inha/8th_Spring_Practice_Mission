b920fb90219547053b94dbf9553056ec46336984
KiSeungMin <ksm011212@gmail.com>
2025-05-26 16:58:41 +0900

    feat: 2번 미션 Repository 메서드 구현 완료

--- src/main/java/umc/study/repository/StoreRepository/StoreRepositoryImpl.java
+++ src/main/java/umc/study/repository/StoreRepository/StoreRepositoryImpl.java
@@ -3,11 +3,17 @@ package umc.study.repository.StoreRepository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import umc.study.domain.Mission;
 import umc.study.domain.QStore;
 import umc.study.domain.Store;
 
 import java.util.List;
+
+import static umc.study.domain.QMission.mission;
 
 @Repository
 @RequiredArgsConstructor
@@ -31,5 +37,37 @@ public class StoreRepositoryImpl implements StoreRepositoryCustom{
                 .selectFrom(store)
                 .where(predicate)
                 .fetch();
+    }
+
+    @Override
+    public Page<Mission> findMissionsByStore(
+            Long storeId,
+            String name,
+            Float minScore,
+            Pageable pageable
+    ) {
+        List<Mission> content = jpaQueryFactory
+                .selectFrom(mission)
+                .join(mission.store, store).fetchJoin()
+                .where(
+                        store.id.eq(storeId)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(mission.id.asc())
+                .fetch();
+
+        Long totalCount = jpaQueryFactory
+                .select(mission.count())
+                .from(mission)
+                .join(mission.store, store)
+                .where(
+                        store.id.eq(storeId)
+                )
+                .fetchOne();
+
+        long total = (totalCount != null ? totalCount : 0L);
+
+        return new PageImpl<>(content, pageable, total);
     }
 }
