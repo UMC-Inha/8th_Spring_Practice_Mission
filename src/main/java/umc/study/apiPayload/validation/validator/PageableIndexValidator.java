402c42f3d83672b33b9e2ab928dca1971f8024e7
KiSeungMin <ksm011212@gmail.com>
2025-05-26 17:34:52 +0900

    chore: 문서 수정

--- src/main/java/umc/study/apiPayload/validation/validator/PageableIndexValidator.java
+++ src/main/java/umc/study/apiPayload/validation/validator/PageableIndexValidator.java
@@ -3,16 +3,15 @@ package umc.study.apiPayload.validation.validator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.data.domain.Pageable;
+import umc.study.apiPayload.validation.annotation.ValidPageableIndex;
 
 public class PageableIndexValidator implements ConstraintValidator<ValidPageableIndex, Pageable> {
     @Override
     public boolean isValid(Pageable pageable, ConstraintValidatorContext context) {
         if (pageable == null) {
-            return true;  // null 처리 정책에 따라 변경
+            return true;
         }
-        int pageNumber = pageable.getPageNumber(); // 0-based
-        // 프론트가 1-based로 보냈다면 getPageNumber()==0 은 page=1,
-        // 음수나 0 이하(프론트가 0이거나 음수 보내면) 검증 실패
+        int pageNumber = pageable.getPageNumber();
         return pageNumber >= 0;
     }
 }
