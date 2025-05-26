79b357a746d42a86ae1320e2296f7d96d4923705
KiSeungMin <ksm011212@gmail.com>
2025-05-26 17:16:52 +0900

    feat: 3번 미션 controller 로직 구현 완료

--- src/main/java/umc/study/web/controller/MemberRestController.java
+++ src/main/java/umc/study/web/controller/MemberRestController.java
@@ -2,17 +2,19 @@ package umc.study.web.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.MemberConverter;
 import umc.study.domain.Member;
 import umc.study.service.MemberService.MemberCommandService;
 import umc.study.web.dto.Member.MemberRequestDTO;
 import umc.study.web.dto.Member.MemberResponseDTO;
+import umc.study.web.dto.Mission.MemberMissionResponseDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +23,15 @@ import umc.study.web.dto.Member.MemberResponseDTO;
 public class MemberRestController {
 
     private final MemberCommandService memberCommandService;
+
+    @GetMapping("/{memberId}/missions")
+    public ApiResponse<Page<MemberMissionResponseDto.JoinResultDTO>> getMemberMissions(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable,
+            @PathVariable("memberId") Long memberId
+    ) {
+        return ApiResponse.onSuccess(memberCommandService.findMemberMissions(memberId, pageable));
+    }
 
     @PostMapping("/")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
