b6e19e25cd87b6eafedb6b65b02adbc9fa55aba1
KiSeungMin <ksm011212@gmail.com>
2025-05-26 17:23:15 +0900

    feat: 4번 미션 controller 로직 구현 완료

--- src/main/java/umc/study/web/controller/MemberMissionRestController.java
+++ src/main/java/umc/study/web/controller/MemberMissionRestController.java
@@ -3,12 +3,10 @@ package umc.study.web.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.MemberMissionConverter;
+import umc.study.domain.enums.MissionStatus;
 import umc.study.domain.mapping.MemberMission;
 import umc.study.service.MemberService.MemberMissionCommandService;
 import umc.study.web.dto.Mission.MemberMissionRequestDto;
@@ -26,5 +24,11 @@ public class MemberMissionRestController {
     public ApiResponse<MemberMissionResponseDto.JoinResultDTO> joinMemberMission(@RequestBody @Valid MemberMissionRequestDto.JoinDto request){
         MemberMission memberMission = memberMissionCommandService.joinMemberMission(request);
         return ApiResponse.onSuccess(MemberMissionConverter.toJoinResultDTO(memberMission));
+    }
+
+    @PostMapping("/{memberMissionId}/complete")
+    public ApiResponse<String> completeMemberMission(@PathVariable("memberMissionId") Long memberMissionId){
+        memberMissionCommandService.changeMemberMissionStatus(memberMissionId, MissionStatus.COMPLETE);
+        return ApiResponse.onSuccess("미션을 완료했습니다.");
     }
 }
