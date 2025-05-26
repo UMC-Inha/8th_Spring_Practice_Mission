c2a6ad1b8ff8e33d0f17af752a624172900d51b5
KiSeungMin <ksm011212@gmail.com>
2025-05-26 17:15:56 +0900

    feat: 3번 미션 service 로직 구현 완료

--- src/main/java/umc/study/service/MemberService/MemberCommandServiceImpl.java
+++ src/main/java/umc/study/service/MemberService/MemberCommandServiceImpl.java
@@ -2,17 +2,22 @@ package umc.study.service.MemberService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import umc.study.apiPayload.code.status.ErrorStatus;
 import umc.study.apiPayload.exception.handler.FoodCategoryHandler;
 import umc.study.converter.MemberConverter;
+import umc.study.converter.MemberMissionConverter;
 import umc.study.converter.MemberPreferConverter;
 import umc.study.domain.FoodCategory;
 import umc.study.domain.Member;
+import umc.study.domain.mapping.MemberMission;
 import umc.study.domain.mapping.MemberPrefer;
 import umc.study.repository.FoodCategoryRepository;
 import umc.study.repository.MemberRepository.MemberRepository;
 import umc.study.web.dto.Member.MemberRequestDTO;
+import umc.study.web.dto.Mission.MemberMissionResponseDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,5 +45,12 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
 
         return memberRepository.save(newMember);
+    }
+
+    @Override
+    public Page<MemberMissionResponseDto.JoinResultDTO> findMemberMissions(Long memberId, Pageable pageable) {
+        Page<MemberMission> memberMissions = memberRepository.findMissionsByMember(memberId, pageable);
+
+        return memberMissions.map(MemberMissionConverter::toJoinResultDTO);
     }
 }
