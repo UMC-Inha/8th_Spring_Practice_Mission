c8aaf3068841af074bbfeeb1de2fc0333e1d3adb
zjhj0814 <zjhj0814@gmail.com>
2025-05-27 21:21:34 +0900

    practice: 9주차 가게 리뷰 조회 실습

---
+++ src/main/java/umc/study/converter/StoreConverter.java
@@ -0,0 +1,35 @@
+package umc.study.converter;
+
+import org.springframework.data.domain.Page;
+import umc.study.domain.Review;
+import umc.study.web.dto.response.ReviewResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StoreConverter {
+
+    public static ReviewResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review) {
+        return ReviewResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+
+    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList) {
+
+        List<ReviewResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return ReviewResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
+                .build();
+    }
+}
