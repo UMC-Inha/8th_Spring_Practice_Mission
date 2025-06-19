8cce583a3b1f864e84c28b8a2dc675fa97ae6f44
seungin <tmddls91@inha.edu>
2025-06-19 17:56:56 +0900

    FEAT: 세션 기반 로그인 구현 완료

--- src/main/java/umc/UMC8th/repository/MemberRepository.java
+++ src/main/java/umc/UMC8th/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package umc.UMC8th.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.UMC8th.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
